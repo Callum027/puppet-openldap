8e26e10b1c488b5f3bb67ea6cfeba64b95226f21
Callum Dickinson <callum@huttradio.co.nz>
2014-07-11 10:04:38 +1200

    No function left behind

--- manifests/server/ldapmodify.pp
+++ manifests/server/ldapmodify.pp
@@ -57,11 +57,12 @@ define openldap::server::ldapmodify
 		group	=> $openldap::params::server_group,
 		mode	=> $openldap::params::server_mode,
 		content	=> template("openldap/ldapmodify.ldif.erb"),
+		require	=> Service[$openldap::params::server_service],
 	}
 
 	exec
 	{ "$openldap::params::ldapmodify -Y EXTERNAL -H ldapi:/// -f $openldap::params::tmpdir/ldapmodify-$title.ldif":
-		require	=> [ Service[$openldap::params::server_service], File["$openldap::params::tmpdir/ldapmodify-$title.ldif"] ],
+		require	=> File["$openldap::params::tmpdir/ldapmodify-$title.ldif"],
 	} ->
 	exec { "$openldap::params::rm -f $openldap::params::tmpdir/ldapmodify-$title.ldif": }
 }
